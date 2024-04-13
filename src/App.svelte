90a3b0555a5692b75d08972e0b74aa45bb66adfb
yuiseki <yuiseki@gmail.com>
2024-04-13 16:31:36 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { MapLibre } from "svelte-maplibre";
+</script>
+
+<main>
+  <MapLibre
+    center={[0, 0]}
+    zoom={4}
+    class="map"
+    standardControls
+    style="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
+  />
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  :global(.map) {
+    height: 100vh;
+  }
+</style>
